73dd45f08c943c74495e003d83910851718469ac
Shubham Agarwal <>
2021-03-12 14:53:44 +0530

    edit code for task2

--- task1.2.js
+++ task1.2.js
@@ -1,17 +1,31 @@
-var fs = require('fs');
+const fs = require('fs');
 const CSVToJSON = require('csvtojson');
+
+const readStream = fs.createReadStream('./csv/csvtojson.csv')
+const writeStream = fs.createWriteStream('./csv/users.txt')
+
+writeStream.on('error', err => {
+    console.log('File write Error :: ', err)
+})
+
+// read line by line
 CSVToJSON()
-  .fromFile('./csv/csvtojson.csv')
-  .then((users) => {
-    console.log(users);
-    fs.writeFile('./csv/users.txt', JSON.stringify(users, null, 4), (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("JSON array is saved.");
-    });
-  })
-  .catch((err) => {
-    // log error if any
-    console.log(err);
-  });
+    .fromStream(readStream)
+    .subscribe((json) => {
+        return new Promise((resolve, reject) => {
+            // create the object to write
+            const objToWrite = {"book": json.Book, "author": json.Author, "price": json.Price}
+            // add a new line to the string
+            writeStream.write(JSON.stringify(objToWrite) + '\n')
+            resolve();
+        })
+    }, onError, onComplete);
+
+function onError(err) {
+    console.log('CSV Error :: ', err)
+}
+
+function onComplete() {
+    console.log('Operation Completed')
+    writeStream.end()
+}
